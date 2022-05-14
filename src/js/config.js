924f4fbc397dbf866889099e64ac1c4965bfe77b
sorinflorian <stefanescu_sorin_florian@yahoo.com>
2022-05-14 23:23:06 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '5d24ecb7-f1db-43e2-b041-ba7b292d8847';
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/views/paginationView.js
@@ -0,0 +1,60 @@
+import View from './View.js';
+import icons from 'url:../../img/icons.svg'; // Parcel 2
+
+class PaginationView extends View {
+  _parentElement = document.querySelector('.pagination');
+
+  addHandlerClick(handler) {
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--inline');
+
+      if (!btn) return;
+
+      const goToPage = +btn.dataset.goto;
+      handler(goToPage);
+    });
+  }
+
+  _generateMarkup() {
+    const curPage = this._data.page;
+    const numPages = Math.ceil(
+      this._data.results.length / this._data.resultsPerPage
+    );
+
+    const nextBtnMarkup = `
+        <button data-goto="${
+          curPage + 1
+        }" class="btn--inline pagination__btn--next">
+          <span>${curPage + 1}</span>
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-right"></use>
+          </svg>
+        </button>
+      `;
+
+    const prevBtnMarkup = `
+        <button data-goto="${
+          curPage - 1
+        }" class="btn--inline pagination__btn--prev">
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-left"></use>
+          </svg>
+          <span>${curPage - 1}</span>
+        </button>
+      `;
+
+    // Page 1, and there are other page
+    if (curPage === 1 && numPages > 1) return nextBtnMarkup;
+
+    // Last page
+    if (curPage === numPages && numPages > 1) return prevBtnMarkup;
+
+    // Other page
+    if (curPage < numPages) return prevBtnMarkup + nextBtnMarkup;
+
+    // Page 1, and there are NO other page
+    return '';
+  }
+}
+
+export default new PaginationView();
